5aef059cbeb7fa38b0489b980a78e88fbc8da906
Sambhram Matahd <125111476+SambhramMathad13@users.noreply.github.com>
2023-09-16 07:14:40 -0900

    Updated  download.php

--- download.php
+++ download.php
@@ -1,32 +1,47 @@
 <?php
-// $class= $_POST['class_name'];
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $class = $_POST['class'];
+    $sd = $_POST['sdd'];
+    $ed = $_POST['edd'];
     include 'connect.php';
     header('Content-Type: text/csv; charset=utf-8');
     header('Content-Disposition: attachment; filename="' . $class . '.csv"');
     $o = fopen('php://output', 'w');
 
-    fputcsv($o, array('DATE'));
-
-
-    $sqll = "SELECT startt,endd FROM `classes` WHERE class_name='$class'";
+    $sqll = "SELECT roll from attendencee WHERE class_name='$class' GROUP BY roll;";
+    
     $ress = mysqli_query($conn, $sqll);
-    $row = mysqli_fetch_assoc($ress);
-    $start = $row['startt'];
-    $endd = $row['endd'];
-    
+    // $row = mysqli_fetch_assoc($ress);
 
-    
-    for ($i = $start; $i <= $endd; $i++) {
-        fputcsv($o, array($i));
-        $sqll = "SELECT DISTINCT datee,`status` FROM attendencee WHERE roll='$i' AND class_name='$class' GROUP BY datee;";
+    $first_row=array('DATE');
+    while ($row = mysqli_fetch_assoc($ress)) {
+            
+        array_push($first_row,$row['roll']);
+    }
+        fputcsv($o,$first_row);
+
+
+        // $sqll = "SELECT DISTINCT datee FROM `attendencee` WHERE class_name='$class';";
+        $sqll = "SELECT DISTINCT datee FROM `attendencee` WHERE class_name='$class' and `datee` between '$sd' and '$ed';";
         $ress = mysqli_query($conn, $sqll);
-
+        $date_ary=array();
         while ($row = mysqli_fetch_assoc($ress)) {
             
-            fputcsv($o, $row);
+            array_push($date_ary,$row['datee']);
         }
+        
+        $len=count($date_ary);
+        for($i=0;$i<$len;$i++) {
+            $d=$date_ary[$i];
+            $a1=array($d);
+        $sqll = "SELECT status from attendencee WHERE datee='$d' AND class_name='$class' GROUP BY roll;";
+        $ress = mysqli_query($conn, $sqll);
+        while ($row = mysqli_fetch_assoc($ress)) {
+            array_push($a1,$row['status']);
+            
+        }
+        fputcsv($o,$a1);
     }
     fclose($o);
 }
+?>
